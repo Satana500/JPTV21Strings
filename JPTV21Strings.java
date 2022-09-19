460ae19efb4e2c39c83386b71f2157decc75074a
Satana500 <113170386+Satana500@users.noreply.github.com>
2022-09-19 15:15:32 +0300

    Update JPTV21Strings.java

--- JPTV21Strings.java
+++ JPTV21Strings.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jptv21strings;
 
-/**
- *
- * @author pupil
- */
-public class JPTV21Strings {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class JPTV21Strings {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Strings ===");
+        System.out.println("Введите текст (латиницей): ");
+        String str = scanner.nextLine();
+        OUTER:
+        do {
+            System.out.println("Выберите номер функции: ");
+            System.out.println("0. Выход из функции");
+            System.out.println("1. Количество символов в тексте");
+            System.out.println("2. Количество слов в тексте");
+            System.out.println("3. Находит указанную подстроку в тексте");
+            System.out.println("4. Заменяет подстроку на другую подстроку");
+            int numberFunction - scanner.nextInt();
+            switch (numberFuction) {
+                case 0;
+                    System.outprintln("Пока!");
+                    repeat = 0;
+                    break;
+                case 1;
+                    System.out.println("");
+                    char[] strChar = 
+            }
+        }
     }
     
 }
